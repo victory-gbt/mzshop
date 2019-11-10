4fe4fc9659e5c1460f2b14ac5ed57fffe089db9d
victory-gbt <57473593+victory-gbt@users.noreply.github.com>
2019-11-10 10:18:18 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
         '/api': {
          target: 'http://localhost:8858',
@@ -59,7 +59,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
